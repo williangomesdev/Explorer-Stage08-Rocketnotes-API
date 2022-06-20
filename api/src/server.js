f7a476acb0b74315047b51314ced03225147f152
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-06-20 08:45:03 -0300

    query params

--- api/src/server.js
+++ api/src/server.js
@@ -1,13 +1,21 @@
+const { request } = require("express");
 const express = require("express");
 const app = express();
 const PORT = 3333;
 
-//GET = pegar resposta
+//GET = pegar resposta/Route params
 app.get("/message/:id/:user", (request, response) => {
   const { id, user } = request.params;
 
   response.send(`id da mensagem: ${id}.
   Para o usuário ${user}`);
+});
+
+//GET = pegar resposta/Query params
+app.get("/users", (request, response) => {
+  const { page, limit } = request.query;
+  response.send(`Página:${page}.
+  Mostrar:${limit}`);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
