3e369728bb25872eb5fc2f6e2902d8b130587e96
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-06-20 17:22:24 -0300

    Método POST

--- api/src/server.js
+++ api/src/server.js
@@ -3,19 +3,9 @@ const express = require("express");
 const app = express();
 const PORT = 3333;
 
-//GET = pegar resposta/Route params
-app.get("/message/:id/:user", (request, response) => {
-  const { id, user } = request.params;
-
-  response.send(`id da mensagem: ${id}.
-  Para o usuário ${user}`);
-});
-
-//GET = pegar resposta/Query params
-app.get("/users", (request, response) => {
-  const { page, limit } = request.query;
-  response.send(`Página:${page}.
-  Mostrar:${limit}`);
+//POST = criação, com envio de dados
+app.post("/users", (request, response) => {
+  response.send("Você chamou o POST");
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
