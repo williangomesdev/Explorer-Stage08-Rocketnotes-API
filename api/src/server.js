9bac148cf08c943190c00815ebe16cbfb453da88
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-06-20 17:32:03 -0300

    body params

--- api/src/server.js
+++ api/src/server.js
@@ -1,11 +1,17 @@
 const { request } = require("express");
 const express = require("express");
 const app = express();
-const PORT = 3333;
+//Informar qual o formato que o servidor enviará os dados
+app.use(express.json());
 
 //POST = criação, com envio de dados
 app.post("/users", (request, response) => {
-  response.send("Você chamou o POST");
+  const { name, email, password } = request.body;
+  /* response.send(`Usuário:${name}, Email:${email}, Password:${password}`); */
+
+  //Responder como json
+  response.json({ name, email, password });
 });
 
+const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
