5e8c1c4dbbc8c7c83ceeb780232018dc1b3edeac
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-06-20 08:25:37 -0300

    Route Params

--- api/src/server.js
+++ api/src/server.js
@@ -1,4 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 3333;
+
+//GET = pegar resposta
+app.get("/message/:id/:user", (request, response) => {
+  const { id, user } = request.params;
+
+  response.send(`id da mensagem: ${id}.
+  Para o usuário ${user}`);
+});
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
