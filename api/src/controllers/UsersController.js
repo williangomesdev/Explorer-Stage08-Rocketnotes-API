3c8cad3942cb499361e7748d331a2d9083e7332c
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-06-20 17:51:31 -0300

    Controllers

---
+++ api/src/controllers/UsersController.js
@@ -0,0 +1,12 @@
+class UsersController {
+  /*um controller pode ter no máximo 5 funções
+  
+  index = GET para listar todos os usuários registrados
+  show = GET exibir um registro especifico
+  create = POST criar um registro
+  update = PUT atualizar um registro
+  delete - DELETE para remover um registro
+  */
+}
+
+module.exports = UsersController;
