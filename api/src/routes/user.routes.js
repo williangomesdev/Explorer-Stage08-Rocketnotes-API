5cfd9f60b1385ca25d068c61dd201f522fbb57f7
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-06-20 22:54:28 -0300

    App Error

--- api/src/routes/user.routes.js
+++ api/src/routes/user.routes.js
@@ -5,23 +5,10 @@ const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router();
 
-//Middleware
-function myMiddleware(request, response, next) {
-  console.log("Você passou pelo middleware!");
-
-  //verificar se a propriedade de administrador do site é verdadeiro para o usuário
-  if (!request.body.isAdmin) {
-    return response.json({ message: "user unauthorized" });
-  }
-
-  //Chama o destino
-  next();
-}
-
 //instanciando na memória
 const usersController = new UsersController();
 
-usersRoutes.post("/", myMiddleware, usersController.create);
+usersRoutes.post("/",usersController.create);
 
 //exportar rotas para o server.js
 module.exports = usersRoutes;
